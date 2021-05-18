52db2d048a3cf6908e33eab86813c5c8fbb8eab0
tomasmonteror <sete25@hotmail.com>
2021-05-18 17:06:26 +0200

    Update MathService.java
    En lugar de multiplicar, suma

--- src/main/java/com/tom/service/MathService.java
+++ src/main/java/com/tom/service/MathService.java
@@ -3,8 +3,7 @@ package com.tom.service;
 public class MathService {
     public int multiply(int firstNumber, int secondNumber) {
 
-        //return firstNumber * secondNumber;
-        return null;
+        return firstNumber + secondNumber;
     }
 
     public int subtract(int firstNumber, int secondNumber) {
