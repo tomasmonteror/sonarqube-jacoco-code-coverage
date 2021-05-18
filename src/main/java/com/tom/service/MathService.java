d5271fc15eedf2099ee424865aa733526d2517e0
tomasmonteror <sete25@hotmail.com>
2021-05-18 17:08:03 +0200

    Update MathService.java
    Ahora multiplica y todo

--- src/main/java/com/tom/service/MathService.java
+++ src/main/java/com/tom/service/MathService.java
@@ -2,8 +2,7 @@ package com.tom.service;
 
 public class MathService {
     public int multiply(int firstNumber, int secondNumber) {
-
-        return firstNumber + secondNumber;
+        return firstNumber * secondNumber;
     }
 
     public int subtract(int firstNumber, int secondNumber) {
