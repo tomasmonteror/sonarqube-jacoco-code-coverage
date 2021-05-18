65742c559f91c7abcda671a2061abd0b1a60a0d3
tomasmonteror <sete25@hotmail.com>
2021-05-18 16:56:29 +0200

    Update MathService.java

--- src/main/java/com/tom/service/MathService.java
+++ src/main/java/com/tom/service/MathService.java
@@ -3,7 +3,8 @@ package com.tom.service;
 public class MathService {
     public int multiply(int firstNumber, int secondNumber) {
 
-        return firstNumber * secondNumber;
+        //return firstNumber * secondNumber;
+        return null;
     }
 
     public int subtract(int firstNumber, int secondNumber) {
